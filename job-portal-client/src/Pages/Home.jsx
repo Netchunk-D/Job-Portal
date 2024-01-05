c3caed220d98170d5a216621911a6fd63db0fe6e
Netchunk-D <vp346915@gmail.com>
2024-01-05 10:46:24 +0530

    Design of main page

--- job-portal-client/src/Pages/Home.jsx
+++ job-portal-client/src/Pages/Home.jsx
@@ -1,10 +1,70 @@
-import React from 'react'
-
+import React,{useEffect, useState} from 'react'
+import Banner from '../components/Banner'
+import Card from '../components/Card';
+import Jobs from './Jobs';
 const Home = () => {
-    
+  const [selectCategory,setSelectCategory] = useState(null);
+  const [jobs,setJobs] = useState([]);
+
+  useEffect(()=>{
+      fetch("jobs.json").then(res => res.json()).then(data=> {
+        // console.log(data);
+        setJobs(data);
+      })
+  },[])
+  // console.log(jobs)
+  
+  {/*handle input change */}
+  const [query,setQuery] = useState("");
+  const handlInputChange = (event) => {
+    setQuery(event.target.value)
+  }
+
+  //filter jobs by title
+  const filteredItems = jobs.filter((job) => job.jobTitle.toLowerCase().indexOf(query.toLowerCase()) !==-1);
+  // console.log(filteredItems)
+
+  // Radio based filtering
+  const handleChange = (event)=>{
+    setSelectCategory(event.target.value)
+  } 
+
+  // button filtering
+  const handleClick =  (event)=>{
+    setSelectCategory(event.target.value)
+  } 
+
+  // main function
+  const filteredData = (jobs,selected,query) => {
+    let filteredjobs =jobs;
+    //filtering input items
+    if(query){
+      filteredjobs = filteredItems;
+    }
+
+    //category selection
+    if(selected){
+      filteredjobs = filteredjobs.filter(({jobLocation,maxPrice,salaryType,experienceLevel,postingDate,employmentType})=> (
+        jobLocation.toLowerCase() === selected.toLowerCase() ||
+        parseInt(maxPrice) <= parseInt(selected) ||
+        salaryType.toLowerCase()=== selected.toLowerCase() ||
+        employmentType.toLowerCase() === selected.toLowerCase() 
+      ));
+      console.log(filteredjobs);
+    }
+    return filteredjobs.map((data,i) => <Card key={i} data={data}/>)
+  }
+
+  const result = filteredData(jobs,selectCategory,query);
   return (
     <div>
-      <h1 className='text-blue'>Home</h1>
+      <Banner query = {query} handleinpchange = {handlInputChange}/>
+      {/* Main containt */}
+      <div className='bg-[#FAFAFA] md:grid grid-cols-4 gap-8 lg:px-24 px-4 py-12'>
+          <div className='bg-white p-4 rounded'>left</div>
+          <div className='col-span-2 bg-white p-4 rounded'><Jobs result = {result}/></div>
+          <div className='bg-white p-4 rounded'>right</div>   
+      </div>
     </div>
   )
 }
--- job-portal-client/src/components/Navbar.jsx
+++ job-portal-client/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
+import { FaBarsStaggered, FaXmark } from "react-icons/fa6";
 const Navbar = () => {
   const [isMenuOpen, SetIsMenuOpen] = useState(false);
   const toggle = () => {
@@ -45,17 +46,54 @@ const Navbar = () => {
         </ul>
         {/*Login and sign in page*/}
         <div className="text-base text-primery font-medium space-x-5 hidden lg:block">
-          <Link to="/login" className="proundedy-2 px-5 border ">
+          <Link to="/login" className="py-2 px-5 border rounded ">
             Login
           </Link>
           <Link
             to="/sign-up"
-            className="proundedy-2 px-5 border bg-blue text-white"
+            className="py-2 px-5 border rounded bg-blue text-white"
           >
             Sign Up
           </Link>
         </div>
+        {/*Mobile menu*/}
+        <div className="md:hidden block">
+          <button onClick={toggle}>
+            {isMenuOpen ? (
+              <FaXmark className="w-5 h-5 text-primery" />
+            ) : (
+              <FaBarsStaggered className="w-5 h-5 text-primery" />
+            )}
+          </button>
+        </div>
       </nav>
+      {/*Navitems for Mobile */}
+      <div
+        className={`px-4 bg-black py-5 rounded-sm ${
+          isMenuOpen ? "" : "hidden"
+        }`}
+      >
+        <ul>
+          {navItems.map(({ path, title }) => (
+            <li
+              key={path}
+              className="text-base text-white first:text-white py-1"
+            >
+              <NavLink
+                to={path}
+                className={({ isActive }) => (isActive ? "active" : "")}
+              >
+                {title}
+              </NavLink>
+            </li>
+          ))}
+          <li className="text-white py-1"><Link to="/login" className="py-2 px-5 rounded ">
+            Login
+          </Link>
+          </li>
+
+        </ul>
+      </div>
     </header>
   );
 };
